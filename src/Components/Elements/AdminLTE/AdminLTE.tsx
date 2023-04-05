abf18b91e09a725600a26521b9dea1a28fa90097
Oliver <olli143@live.de>
2023-04-05 13:41:25 +0200

    add prettier to eslint

--- src/Components/Elements/AdminLTE/AdminLTE.tsx
+++ src/Components/Elements/AdminLTE/AdminLTE.tsx
@@ -1,15 +1,15 @@
 import { IAdminLTEBase } from "../../../Types/BaseTypes";
 
-export function LTERibbon( Props : IAdminLTEBase ) {
-	if ( Props.Hide ) {
-		return <></>;
-	}
+export function LTERibbon(Props: IAdminLTEBase) {
+  if (Props.Hide) {
+    return <></>;
+  }
 
-	return (
-		<div className="ribbon-wrapper" { ...Props }>
-			<div className={ `ribbon bg-${ Props.Color || "danger" }` }>
-				{ Props.children }
-			</div>
-		</div>
-	);
+  return (
+    <div className="ribbon-wrapper" {...Props}>
+      <div className={`ribbon bg-${Props.Color || "danger"}`}>
+        {Props.children}
+      </div>
+    </div>
+  );
 }
