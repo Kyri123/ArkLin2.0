fc7f412f18f2d4afb1560b93c91c08021c356e3e
Oliver <olli143@live.de>
2023-04-10 09:26:05 +0200

    remove old content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,7 @@ module.exports = {
 		'./index.html',
 		'./src/**/*.{js,jsx,ts,tsx}'
 	],
-	darkMode: 'media',
 	theme: {
-		extend: {},
-		screens: {
-			'sm': '576px',
-			'md': '768px',
-			'lg': '992px',
-			'xl': '1200px',
-		}
-	},
-	variants: {
 		extend: {},
 	},
 	plugins: [],
