7ed32ea7927cfde0e0a08521ed70974754842ae5
Kyrium <olli143@live.de>
2023-04-26 07:12:36 +0200

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -13,14 +13,27 @@ mkdir KAdmin
 cd KAdmin
 rm -R ArkLin2.0 -f
 git clone https://github.com/Kyri123/ArkLin2.0.git ArkLin2.0
-
-echo "Create mount folder on steam user"
-./sh/folder.sh
-
 cd ArkLin2.0
 
 git fetch --all
 git reset --hard origin/$BRANCH
+
+echo "Create mount folder on steam user"
+mkdir -p -v ../mount/config
+mkdir -p -v ../mount/Server
+mkdir -p -v ../mount/Backups
+mkdir -p -v ../mount/PanelLogs
+mkdir -p -v ../mount/Logs
+mkdir -p -v ../mount/mongodb
+
+chmod 777 -R ./sh
+chmod 777 -R ../mount/config
+chmod 777 ../mount/Server
+chmod 777 ../mount/Backups
+chmod 777 ../mount/mongodb
+chmod 777 ../mount/Logs
+chmod 777 -R ../mount/PanelLogs
+chmod 777 -R /etc/arkmanager/
 
 mv docker-compose.yml.example docker-compose.yml
 
