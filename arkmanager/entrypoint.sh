2e2b0a3e38a1a8faf799dbde6aa003c2f1570693
Arthur Mayer <mayer.arthur@gmail.com>
2023-06-10 22:13:40 +0000

    dockerizing arkmanager part 5 (steamcmd testing)

--- arkmanager/entrypoint.sh
+++ arkmanager/entrypoint.sh
@@ -6,4 +6,10 @@ printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
 service ssh start
 service ssh status
 
+# installation as root
+steamcmd +quit
+
+# installation as steam
+su -c '/home/steam/steamcmd +quit' steam
+
 exec "$@"
